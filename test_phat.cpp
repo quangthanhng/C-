641a24dac21ad3137c6e6a8b83ad2a39932ca04f
Thanh <quangthanh.nguyen2512@gmail.com>
2024-10-04 23:41:48 +0700

    thay doi file

--- test_phat.cpp
+++ test_phat.cpp
@@ -1,24 +1,7 @@
-#include <iostream>
-#include <string>
+#include<iostream>
 using namespace std;
 
-class Employee {
-    public:
-    string name;
-    void set_salary(const double potential_salary){
-        salary = potential_salary;
-    }
-    double get_salary(){
-        return salary;
-    }
-    private:
-    double salary;
-};
-
 int main(){
-    Employee nv1;
-    nv1.name = "Kevin";
-    cout << nv1.name << endl;
-    nv1.set_salary(50000);
-    cout << nv1.get_salary() << endl;
+    char name[] = "Thanh";
+    cout << name;
 }
